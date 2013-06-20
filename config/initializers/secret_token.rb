fde52fbcf5396db12fc2fe23b222e1abdb7a7de8
clovits <clovits@gmail.com>
2013-06-20 13:38:30 -0300

    Initial Commit of ControlaLojaWeb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ControlaLojaWeb::Application.config.secret_token = 'a37700d6d7b3ce1cb8f1e7abfe1041caf82bce2a6c25e5668ab7cde38269f20906f206d56d1657accb730aba4106a585ab7812a4ba086b7b7fa12f13ed448d30'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ControlaLojaWeb::Application.config.session_store :cookie_store, key: '_ControlaLojaWeb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ControlaLojaWeb::Application.config.session_store :active_record_store
